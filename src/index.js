66f1319c0a6605cbc424a08c56ea8e2852407705
ysmolyar <yonismolyar1@gmail.com>
2018-07-16 14:31:23 -0400

    linked index to bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 ReactDOM.render(
     <h1>Welcome to the world's simplest webapp using react!</h1>,
     document.getElementById('root')
