979c5920a8ca2667dac5b6b219a81e7de34bfebb
ysmolyar <yonismolyar1@gmail.com>
2018-07-17 19:46:33 -0400

    made course-card separate file

--- src/index.js
+++ src/index.js
@@ -10,24 +10,6 @@ import ModuleListItem from './components/ModuleListItem'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 
-class CourseCard extends React.Component {
-    render() {
-        return (
-
-            <div className="card" styles={{width: '18rem'}}>
-                <img className="card-img-top" alt={"Card image"}
-                     src="https://picsum.photos/300/200"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Card text.</p>
-                    <a href="#" className="btn btn-primary">More...</a>
-                </div></div>
-
-        )
-    }
-}
-
-
 
 class BeigeBoard extends React.Component {
     render() {
