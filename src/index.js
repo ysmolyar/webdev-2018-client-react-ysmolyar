954f0889b9631332c615300cb8362e14b23bc806
ysmolyar <yonismolyar1@gmail.com>
2018-07-17 02:38:03 -0400

    removed hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './hello'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import LessonTabs from './LessonTabs'
