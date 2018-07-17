7f74ce21e811ce7ac6d591ec8d803eee76e99227
ysmolyar <yonismolyar1@gmail.com>
2018-07-17 18:54:03 -0400

    modules added to components

---
+++ src/components/ModuleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ModuleListItem from "ModuleListItem";
+
+class ModuleList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Module List</h1>
+                <ul className="list-group">
+                    <ModuleListItem title="Module 1"/>
+                    <ModuleListItem title="Module 2"/>
+                    <ModuleListItem title="Module 3"/>
+                    <ModuleListItem title="Module 4"/>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ModuleList;
