faec23a294775694813e02629cd66dd269306eaf
ysmolyar <yonismolyar1@gmail.com>
2018-07-17 21:39:04 -0400

    made proper course manager

---
+++ src/components/CourseManager.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CourseCard from './CourseCard';
+
+class CourseManager extends React.Component {
+    render() {
+        return (
+            <div class="container-fluid">
+                <h1>Beigeboard</h1>
+                <div class="card-deck">
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                </div>
+            </div>
+        )
+    }
+}
