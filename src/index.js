ce69fb39f42d2a740c651860c76c596bd52ad967
ysmolyar <yonismolyar1@gmail.com>
2018-07-17 21:37:30 -0400

    made directory changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import LessonTabs from './components/LessonTabs'
 import TopicPills from './components/TopicPills'
 import CourseList from './components/CourseList'
+import CourseManager from './components/CourseManager'
 import ModuleList from './components/ModuleList'
 import CourseCard from './components/CourseCard'
 import ModuleListItem from './components/ModuleListItem'
@@ -17,10 +18,10 @@ class BeigeBoardRouter extends React.Component {
         return (
             <Router>
                 <div>
-                    <Link to="/components/CourseList">Course List</Link>
-                    <Route exact={true} path='/' render={() => <BeigeBoard/>}/>
-                    <Route path='/components/CourseList'
-                           component={CourseList}/>
+                    <Link to="/">Link</Link>
+                    <Route exact={true} path='/' render={() => <CourseManager/>}/>
+                    <Route path='/'
+                           component={CourseManager}/>
                 </div>
             </Router>
         )
@@ -29,22 +30,6 @@ class BeigeBoardRouter extends React.Component {
 }
 
 
-class BeigeBoard extends React.Component {
-    render() {
-        return (
-            <div class="container-fluid">
-                <h1>Beigeboard</h1>
-                <div class="card-deck">
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                    <CourseCard/>
-                </div>
-            </div>
-        )
-    }
-}
 
 
 
