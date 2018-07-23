686c2c6554eb7d9ed60269f6e6d647ab7309f209
ysmolyar <yonismolyar1@gmail.com>
2018-07-22 23:47:46 -0400

    added dynamic render function to courseList, still not connected to back-end

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,27 +1,35 @@
 import React, {Component} from "react";
-import ModuleListItem from "./ModuleListItem";
+import ModuleListItem from "../components/ModuleListItem";
 import CourseServiceClient from '../services/CourseServiceClient'
 
 class CourseList extends React.Component {
-    // var courseService = new CourseServiceClient;
 
-    constructor(props) {super(props);}
+    constructor() {
+        super();
+        this.state = {
+            modules: [
+                {title: 'Module 1 - jQuery', id: 123},
+                {title: 'Module 2 - React', id: 234},
+                {title: 'Module 3 - Redux', id: 345},
+                {title: 'Module 4 - Angular', id: 456},
+                {title: 'Module 5 - Node.js', id: 567},
+                {title: 'Module 6 - MongoDB', id: 678},]};}
 
-    // createCourse = () => {
-    //     this.courseService.createCourse(this.state.newCourse)
-    //         .then((course) => this.courseService.findAllCourses())
-    //         .then(courses => {this.setState({courses: courses})})
-    // };
+
+    renderListOfModules() {
+        let modules = this.state.modules
+            .map(function(module){
+                return <ModuleListItem
+                    title={module.title} key={module.id}/>
+            });
+        return modules;}
+
 
     render() {
         return (
             <div>
-                <ul className="list-group container-fluid" >
-                    <input className="form-control" placeholder="Course Name"/>
-                    <ModuleListItem title="Course 1"/>
-                    <ModuleListItem title="Course 2"/>
-                    <ModuleListItem title="Course 3"/>
-                    <ModuleListItem title="Course 4"/>
+                <ul className="col-sm-10 courseList list-group container-fluid" >
+                    {this.renderListOfModules()}
                 </ul>
             </div>
         )
