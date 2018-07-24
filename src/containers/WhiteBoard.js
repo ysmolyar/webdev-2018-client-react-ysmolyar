485620185f5b0e90a98ee5d44e6cd71ce2bda8c1
ysmolyar <yonismolyar1@gmail.com>
2018-07-23 23:33:38 -0400

    added return to whiteboard

--- src/containers/WhiteBoard.js
+++ src/containers/WhiteBoard.js
@@ -1,17 +1,18 @@
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import React, {Component} from "react";
-import CourseServiceClient from '../services/CourseServiceClient'
-import CourseRow from "../components/CourseRow";
+
 import "../styles.css";
 import "../styles/CourseList.css"
 
 export default class WhiteBoard extends React.Component {
 
     render() {
-        <div>
+        return (
+        <div onClick={() => window.location.href = "/courses"}>
             <button className="btn-success" value="Go to Course Manager"></button>
         </div>
+        )
     }
 
 }
